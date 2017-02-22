fcb8dda043d0c69f6339ee388e7d0e6bbc8a755b
jacobrave <Jacob.Rave@loop.colum.edu>
2017-02-22 01:07:20 -0600

    Added source.

---
+++ Script/CubemapRender.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubemapRender : MonoBehaviour {
+
+	public int cubemapSize = 128;
+	public float nearClip = 0.01f;
+	public float farClip = 500;
+	public bool oneFacePerFrame = false;
+	public LayerMask layerMask;
+	Camera cam;
+	RenderTexture rTex;
+
+	void Start () {
+		UpdateCubemap (63);
+	}
+
+	void UpdateCubemap(int faceMask)
+	{
+		if (!cam)
+		{
+			GameObject cmc = new GameObject("CubemapCamera", typeof(Camera));
+			cmc.hideFlags = HideFlags.HideAndDontSave;
+			cmc.transform.position = transform.position;
+			cmc.transform.rotation = Quaternion.identity;
+
+			cam = cmc.GetComponent<Camera>();
+			cam.cullingMask = layerMask;
+			cam.nearClipPlane = nearClip;
+			cam.farClipPlane= farClip;
+			cam.fieldOfView = 12.0f;
+			cam.enabled = false;
+		}
+
+		if (!rTex)
+		{
+			rTex = new RenderTexture(cubemapSize, cubemapSize, 16);
+			rTex.isPowerOfTwo = true;
+			rTex.isCubemap = true;
+			rTex.hideFlags = HideFlags.HideAndDontSave;
+
+			foreach(Renderer r in GetComponentsInChildren<Renderer>())
+			{
+				foreach (Material m in r.sharedMaterials)
+				{
+					if (m.HasProperty("_Cube"))
+					{
+						m.SetTexture("_Cube",rTex);
+					}
+				}
+			}
+		}
+		cam.transform.position = transform.position;
+		cam.RenderToCubemap (rTex, faceMask);
+	}
+
+	void LateUpdate()
+	{
+		if (oneFacePerFrame) {
+			int faceToRender = Time.frameCount % 6;
+			int faceMask = 1 << faceToRender; //cubemap faces are a bitfield.
+			UpdateCubemap (faceMask);
+		} else
+		{
+			UpdateCubemap (63);
+		}
+	}
+
+	void OnDisable()
+	{
+		DestroyImmediate (cam);
+		DestroyImmediate (rTex);
+	}
+}
---
+++ Shader/TessReflect.shader
@@ -0,0 +1,64 @@
+﻿Shader "Custom/TessReflect" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_Cube ("Reflection Cubemap", Cube) = "" { TexGen CubeReflect }
+		_Displacement ("Displacement", Range(-1.0, 1.0)) = 0
+		_Tess ("Tesselation", Range(1,32)) = 4
+		_DispTex ("Displacement Texture", 2D) = "gray" {}
+		
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 300
+		
+		CGPROGRAM
+		
+		#pragma surface surf BlinnPhong addshadow fullforwardshadows vertex:disp tessellate:tessFixed nolightmap
+		#pragma target 5.0
+		
+		struct appdata{
+			float4 vertex: POSITION;
+			float4 tangent: TANGENT;
+			float3 normal: NORMAL;
+			float2 texcoord: TEXCOORD0;
+		};
+
+		float _Tess;
+
+		float4 tessFixed()
+		{
+			return _Tess;
+		}
+		
+		sampler2D _DispTex;
+		float _Displacement;
+		samplerCUBE _Cube;
+		fixed4 _Color;
+		
+		void disp(inout appdata v){
+			float d = tex2Dlod(_DispTex, float4(v.texcoord.xy, 0, 0)).r * _Displacement;
+			v.vertex.xyz += v.normal * d;
+		}
+
+		struct Input 
+		{
+			float2 uv_MainTex;
+			float3 worldRefl;
+		};
+		
+		sampler2D _MainTex;
+		sampler2D _NormalMap;
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			fixed4 c =  _Color;
+			o.Albedo = c.rgb;
+			fixed4 reflcol = texCUBE (_Cube, IN.worldRefl);
+			o.Specular = fixed4(1.0,1.0,1.0,1.0);
+			o.Albedo = reflcol.rgb;
+			o.Alpha = reflcol.a;
+		}
+		
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
